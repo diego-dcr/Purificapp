e561bc6ad51dd248530d64cf99b22a26160c1512
diego-dcr <omenplus.ddrc@gmail.com>
2026-04-08 13:37:28 -0600

    Verificacion de Factories para Laravel Cloud

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,7 @@ class DatabaseSeeder extends Seeder
         app(PermissionRegistrar::class)->forgetCachedPermissions();
 
         $this->seedBaseData();
-
-        if ($this->shouldSeedDemoData()) {
-            $this->call(DemoDataSeeder::class);
-        }
+        $this->call(DemoDataSeeder::class);
     }
 
     private function seedBaseData(): void
@@ -42,17 +39,5 @@ class DatabaseSeeder extends Seeder
         $admin->syncRoles(['admin']);
 
         $this->call(ConceptSeeder::class);
-    }
-
-    private function shouldSeedDemoData(): bool
-    {
-        $profile = strtolower((string) env('SEED_PROFILE', 'base'));
-
-        if (in_array($profile, ['demo', 'all'], true)) {
-            return true;
-        }
-
-        // Backward compatibility with previous toggle.
-        return filter_var(env('SEED_DEMO_DATA', false), FILTER_VALIDATE_BOOL);
     }
 }
